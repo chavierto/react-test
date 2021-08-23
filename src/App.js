f48314e6c6c3341069e31b89d0df99a82a6c8df1
chavierto <hola@xavierlujan.com>
2021-08-23 03:00:20 +0200

    Create KanyeDaily component
        - Add header and placeholder for quote
        - Add KanyeDaily to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import KanyeDaily from './KanyeDaily';
 
 function App() {
 	return (
 		<div className='App'>
 			<h1>React Test</h1>
+			<KanyeDaily></KanyeDaily>
 		</div>
 	);
 }
---
+++ src/KanyeDaily.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function KanyeDaily(props) {
+	const quotes = props;
+	return (
+		<div>
+			<h3>Kanye Of The Day</h3>
+			<div className='quote'>Quote</div>
+		</div>
+	);
+}
+export default KanyeDaily;
