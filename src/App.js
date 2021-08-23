0190e197f5ead20e7908ca6c428aeb03381c754b
chavierto <hola@xavierlujan.com>
2021-08-23 03:33:44 +0200

    Install axios, set kanyeQuote to state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import KanyeDaily from './KanyeDaily';
 
 function App() {
+	const [kanyeQuote, setKanyeQuote] = useState([]);
+
+	useEffect(() => {
+		async function getQuote() {
+			const quote = await axios
+				.get(`https://api.kanye.rest/`)
+				.catch((err) => console.log(err));
+			setKanyeQuote(quote);
+		}
+		getQuote();
+	}, []);
+
 	return (
 		<div className='App'>
 			<h1>React Test</h1>
